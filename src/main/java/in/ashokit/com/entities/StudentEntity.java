41ed105427e25169d82ac222b8cc95844493031e
SHUBH9130 <shubhamsone007@gmail.com>
2021-11-12 11:23:03 +0530

    Student Enitity added

--- src/main/java/in/ashokit/com/entities/StudentEntity.java
+++ src/main/java/in/ashokit/com/entities/StudentEntity.java
@@ -2,7 +2,20 @@ package in.ashokit.com.entities;
 
 public class StudentEntity {
 
+	private Integer sid;
+	private String sname;
+	
+	public void showData(Integer sid,String sname) {
+	this.sid=sid;
+	this.sname=sname;
+		System.out.println("Student ID "+sid+" Student Name "+sname);
+	}
+	
+	
 	public StudentEntity() {
 	System.out.println("StudentEntity.StudentEntity()");
+	
+	
+	
 	}
 }
